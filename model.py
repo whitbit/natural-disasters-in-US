efa73479b8cc00237d111048b0a603d1bb0d3320
Whitney <whitbyte@gmail.com>
2018-01-09 22:01:12 +0000

    added function to extract pertinent data from csv file

--- model.py
+++ model.py
@@ -20,6 +20,10 @@ def connect_to_db(app, db_uri=None):
 
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri or 'postgres:///natural_disasters'
     app.config['SQLCHEMY_ECHO'] = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     db.init_app(app)
 
+if __name__ == '__main__':
+    from server import app
+    connect_to_db(app)
---
+++ seed.py
@@ -0,0 +1,19 @@
+from model import NaturalDisasters, connect_to_db, db
+from server import app
+import csv
+
+def get_natural_disasters_data(csv_file):
+
+    events = []
+
+    with open(csv_file) as data:
+        disaster_data = csv.reader(data)
+
+        for row in disaster_data:
+            events.append({ 'state': row[5],
+                            'date': row[10],
+                            'type': row[8]})
+
+    return events
+
+print get_natural_disasters_data('DisasterDeclarationsSummaries.csv')
