0f779a7107e434b1750f5844cd22218f0a295147
Whitney <whitbyte@gmail.com>
2018-01-14 06:59:58 +0000

    cleaned up styling

--- static/display-map.js
+++ static/display-map.js
@@ -43,14 +43,14 @@ function renderMap() {
             [100, '#FC4E2A'],
             [200, '#E31A1C'],
             [500, '#BD0026'],
-            [1000, '#000000']
+            [1000, '#500018']
         ]
       },
       'fill-opacity': 0.6
     }
   }, firstSymbolId);
 
-  buildLegend();
+
 }
 
 map.on('click', 'us-states', function(e) {
@@ -70,25 +70,7 @@ function almostFlatten(arr) {
   }, []);
 }
 
-function buildLegend() {
 
-  var layers = ['0-10', '10-20', '20-50', '50-100', '100-200', '200-500', '500-1000', '1000+'];
-  var colors = ['#FFEDA0', '#FED976', '#FEB24C', '#FD8D3C', '#FC4E2A', '#E31A1C', '#BD0026', '#800026'];
-
-  for (i = 0; i < layers.length; i++) {
-    var layer = layers[i];
-    var color = colors[i];
-    var item = document.createElement('div');
-    var key = document.createElement('span');
-    key.className = 'legend-key';
-    key.style.backgroundColor = color;
-  }
-  var value = document.createElement('span');
-  value.innerHTML = layer;
-  item.appendChild(key);
-  item.appendChild(value);
-  legend.appendChild(item);
-}
 
 map.on('mousemove', function(e) {
   var states = map.queryRenderedFeatures(e.point, {
@@ -96,9 +78,9 @@ map.on('mousemove', function(e) {
   });
 
   if (states.length > 0) {
-    // $('#pd').append('<h2>' + states[0].properties.name + '</h3>' + states[0].properties.density + 'occurrence(s)')
-    document.getElementById('pd').innerHTML = '<h3><strong>' + states[0].properties.name + '</strong></h3><p><strong><em>' + states[0].properties.density + '</strong> people per square mile</em></p>';
+    document.getElementById('pd').innerHTML = '<h3><strong>' + states[0].properties.name + '</strong></h3> \
+                                              <p><strong><em>' + states[0].properties.density + '</strong> occurence(s)</em></p>';
   } else {
-    document.getElementById('pd').innerHTML = '<p>Hover over a state!</p>';
+    document.getElementById('pd').innerHTML = '<p>Hover over a state or scroll down for table details!</p>'
   }
 });
